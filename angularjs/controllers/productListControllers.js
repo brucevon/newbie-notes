dd407971b05928a88f5b33b12d2d70a8925132a8
brucevon <brucevon@qq.com>
2019-10-22 14:51:31 +0800

    feat(*): 6.3添加分页

--- angularjs/controllers/productListControllers.js
+++ angularjs/controllers/productListControllers.js
@@ -1,16 +1,30 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass) {
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
 
         var selectCategory = null;
 
+        $scope.selectedPage = 1;
+
+        $scope.pageSize = productListPageCount;
+
         $scope.selectCategory = function (newCategory) {
             selectCategory = newCategory;
-        }
+            $scope.selectedPage = 1;
+        };
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        };
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
+        };
 
         $scope.categoryFilterFn = function (product) {
             return selectCategory == null || product.category == selectCategory;
-        }
+        };
 
         $scope.getCategoryClass = function (category) {
             return selectCategory == category ? productListActiveClass : "";
